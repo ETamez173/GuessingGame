d4d7b190a4fcd99e162ea718f5a01e13f88da1d3
ETamez9173 <elitamez17@gmail.com>
2020-02-27 21:46:41 -0600

    got the game to take 3 attempts and give higher lower hints

--- Program.cs
+++ Program.cs
@@ -13,16 +13,25 @@ namespace GuessingGame {
                 Console.WriteLine ("Guess a number between 1 and 10");
                 string userGuess = Console.ReadLine ();
                 int userGuessInt = int.Parse (userGuess);
-                if (userGuessInt == secretNumber) {
+
+                if (userGuessInt > secretNumber && i != 2) {
+                    Console.WriteLine ("Enter smaller number");
+
+                } else if (userGuessInt < secretNumber && i != 2)
+
+                {
+                    Console.WriteLine ("Enter higher number");
+                } else if (i == 2 && userGuessInt != secretNumber)
+
+                {
+                    Console.WriteLine ($"Sorry Game is Over! It was {secretNumber} ");
+                } else {
                     Console.WriteLine ($"You guessed it! It was {secretNumber}");
                     return;
-                } else {
-                    Console.WriteLine ($"Nope that wasnt it! ");
 
                 }
 
             }
-
         }
     }
 }
